c09b6136a0aa6dcb4668d61bf5050116745bab06
Svyatoshka <30661505+Svyatoshka@users.noreply.github.com>
2019-12-16 17:42:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
-const {
-	prefix
-} = require('./config.json');
+const prefix = require('./config.json');
 const ytdl = require('ytdl-core');
 
 const client = new Discord.Client();
